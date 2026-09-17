21081d891cdae457861dc8a6c6c71d73b7c07242
jyothiradithyab34-a11y <jyothiradithyab34@gmail.com>
2026-09-17 19:26:11 +0530

    deleting the existing files

--- syncdoc-backend/src/server.js
+++ syncdoc-backend/src/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
-const connectDB = require("./config/db");
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+
+const connectDB = require("./config/db");
+const collaborationSocket = require("./sockets/collaboration.socket");
 
 const app = express();
 
@@ -18,8 +20,8 @@ const server = http.createServer(app);
 // Create Socket.io server
 const io = new Server(server, {
   cors: {
-    origin: "*"
-  }
+    origin: "*",
+  },
 });
 
 // Test API route
@@ -27,18 +29,14 @@ app.get("/", (req, res) => {
   res.send("SyncDoc backend is running!");
 });
 
-// Socket.io connection
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
+// Initialize collaboration socket
+collaborationSocket(io);
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 connectDB();
+
 server.listen(PORT, () => {
   console.log(`SyncDoc server running on http://localhost:${PORT}`);
 });
